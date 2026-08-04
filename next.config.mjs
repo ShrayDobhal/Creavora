1a73b1b2f483df6b1b3564c3929c06e9cd14cd21
Shray Dobhal <shraydobhal255@gmail.com>
2026-08-04 22:17:06 +0530

    fix: allow Vercel deployment preview framing

--- next.config.mjs
+++ next.config.mjs
@@ -25,11 +25,10 @@ const nextConfig = {
         headers: [
           { key: "X-DNS-Prefetch-Control", value: "on" },
           { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
-          { key: "X-Frame-Options", value: "SAMEORIGIN" },
           { key: "X-Content-Type-Options", value: "nosniff" },
           { key: "Referrer-Policy", value: "origin-when-cross-origin" },
           { key: "X-XSS-Protection", value: "1; mode=block" },
-          { key: "Content-Security-Policy", value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; media-src 'self' https:; connect-src 'self' ws: wss: https:;" }
+          { key: "Content-Security-Policy", value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; media-src 'self' https:; connect-src 'self' ws: wss: https:; frame-ancestors 'self' https://vercel.com https://*.vercel.com;" }
         ]
       }
     ];
